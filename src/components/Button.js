5df994744c5e83ddd32c26fdc9f3075bdc9186fd
Hanna L <hannalee92@gmail.com>
2022-12-15 15:40:59 -0500

    added onClick for btn

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,30 @@
+import PropTypes from 'prop-types'
+
 const Button = ({ color, text }) => {
-  return <button style={{ backgroundColor: color }} 
-  className='btn'>{text}</button>
+    const onClick = () => {
+}    
+  return (
+    <button 
+        onClick={onClick} 
+        style={{ backgroundColor: color }} 
+        className='btn'
+    >
+        {text}
+    </button>
+)
 }
+
+
+Button.default.Props = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+}
+
+
+
 
 export default Button
